a64bb206eb72e5c0abdd10c820d72d8b1dc30d28
Dennis Lau <dennis@Denniss-MacBook-Pro.local>
2021-11-08 19:41:56 +0800

    feat: update multiprocessing call

--- src/util/multiprocessing.py
+++ src/util/multiprocessing.py
@@ -2,24 +2,50 @@ from multiprocessing import Pool
 from tqdm import tqdm
 from model.time_series import TimeSeries
 from functools import partial
+from typing import Iterable, Callable, TypeVar
+from model.model_setting import BaseModelSetting
 
 
-def precal(base_path: str, filename: str) -> TimeSeries:
+# Declare Generic
+T = TypeVar('T')
+
+
+def precal(
+        filename: str,
+        base_path: str,
+        prediction_models: list[BaseModelSetting]) -> TimeSeries:
+    '''
+    Call TimeSeries.int_plot() to trigger calculation for all `BaseModelSetting`
+    '''
     ts = TimeSeries(base_path, filename)
-    # ts.anomalies_precal()
+    ts.prediction_models = prediction_models
     ts.int_plot()
     return ts
 
 
-def mp_precal(base_path, filenames) -> list[TimeSeries]:
-    'Multiprocess TimeSeries precal with progress bar'
-    ts_list: list[TimeSeries] = []
+def mp_process(
+        *,
+        func: Callable[..., T],
+        iterable: Iterable,
+        chunksize: int = 1,
+        **kwargs) -> list[T]:
+    '''
+    Wrapper for multiprocessing a func, returns an unordered list of generic type T.
+    func: takes a single positional arg (an element from `iterable`) and `**kwargs`, returns T. Element from `iterable` must be the first arg.
+    iterable: each element will be fed into `func` as positional arg
+    chunksize: chunksize of imap_unordered, modify according to task
+    '''
+    T_list: list[T] = []
     # by default uses num of cpu
     with Pool() as pool:
         # dynamic progress bar for ipython
         for ts in tqdm(
-            pool.imap_unordered(partial(precal, base_path), filenames),
-            total=len(filenames)
+            pool.imap_unordered(
+                partial(func, **kwargs),
+                iterable,
+                chunksize=chunksize
+            ),
+            total=len(iterable)
         ):
-            ts_list.append(ts)
-    return sorted(ts_list)
+            T_list.append(ts)
+    return T_list
