8b40537c9f1d777cd2be1edbfbce0541c4136151
Hayden2018 <39459225+Hayden2018@users.noreply.github.com>
2021-11-23 17:10:36 +0800

    pre-trained parameters

--- py/hayden_regression.py
+++ py/hayden_regression.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import torch
 from torch.optim import Adam
 from torch import nn
+import os
 
 class ConvModel(nn.Module):
 
-    def __init__(self, linear_size=960):
+    def __init__(self, linear_size=1440):
         super(ConvModel, self).__init__()
-        self.conv_left = nn.Conv1d(1, 8, 5)
-        self.conv_right = nn.Conv1d(1, 8, 5)
+        self.conv_left = nn.Conv1d(1, 12, 5)
+        self.conv_right = nn.Conv1d(1, 12, 5)
         self.linear = nn.Linear(linear_size, 1)
         self.act = nn.LeakyReLU(negative_slope=0.05)
 
@@ -37,56 +38,59 @@ def create_regression_set(arr, outer=64, inner=16, delta=1):
         pivot += delta
     return x_left, x_right, y
 
-
 BASE_PATH = './data-sets/KDD-Cup/data/'
-df = pd.read_csv(BASE_PATH + '001_UCR_Anomaly_35000.txt', names=['values'])
-plt.figure(figsize=[20, 5])
-plt.plot(df['values'])
-plt.show()
+for fname in os.listdir('./data-sets/KDD-Cup/data/'):
+    df = pd.read_csv(BASE_PATH + fname, names=['values'])
+    print('Now processing:', fname)
+    # plt.figure(figsize=[20, 5])
+    # plt.plot(df['values'])
+    # plt.show()
 
-series = df['values'].to_numpy()
-x_left, x_right, y = create_regression_set(series)
+    series = df['values'].to_numpy()
+    x_left, x_right, y = create_regression_set(series)
 
-model = ConvModel()
-compute_loss = nn.L1Loss(reduction='sum')
-optimizer = Adam(model.parameters(), lr=0.001)
+    model = ConvModel()
+    compute_loss = nn.MSELoss(reduction='mean')
+    optimizer = Adam(model.parameters(), lr=0.0005)
 
-batch_size = 64
-num_epoch = 12
+    batch_size = 64
+    num_epoch = 15
 
-for j in range(num_epoch):
-    print(f'Training {j + 1} Epoch...')
-    loss_sum = 0
-    for i in range(0, len(y) - batch_size, batch_size):
-        optimizer.zero_grad()
-        batch_y = torch.tensor(y[i:i + batch_size], dtype=torch.float32).view(batch_size, 1)
-        batch_left = x_left[i:i + batch_size]
-        batch_left = torch.tensor(batch_left, dtype=torch.float32).view(batch_size, 1, -1)
-        batch_right = x_right[i:i + batch_size]
-        batch_right = torch.tensor(batch_right, dtype=torch.float32).view(batch_size, 1, -1)
-        pred = model(batch_left, batch_right)
-        loss = compute_loss(pred, batch_y)
-        loss.backward()
-        optimizer.step()
-        loss_sum += float(loss)
-    print(f'Epoch Loss: {loss_sum / len(y)}')
+    for j in range(num_epoch):
+        print(f'Training {j + 1} Epoch...')
+        loss_sum = 0
+        for i in range(0, len(y) - batch_size, batch_size):
+            optimizer.zero_grad()
+            batch_y = torch.tensor(y[i:i + batch_size], dtype=torch.float32).view(batch_size, 1)
+            batch_left = x_left[i:i + batch_size]
+            batch_left = torch.tensor(batch_left, dtype=torch.float32).view(batch_size, 1, -1)
+            batch_right = x_right[i:i + batch_size]
+            batch_right = torch.tensor(batch_right, dtype=torch.float32).view(batch_size, 1, -1)
+            pred = model(batch_left, batch_right)
+            loss = compute_loss(pred, batch_y)
+            loss.backward()
+            optimizer.step()
+            loss_sum += float(loss)
+        print(f'Epoch Loss: {loss_sum}')
 
-residual = []
-for i in range(len(y)):
-    batch_y = torch.tensor(y[i], dtype=torch.float32).view(1, 1)
-    batch_left = x_left[i]
-    batch_left = torch.tensor(batch_left, dtype=torch.float32).view(1, 1, -1)
-    batch_right = x_right[i]
-    batch_right = torch.tensor(batch_right, dtype=torch.float32).view(1, 1, -1)
-    pred = model(batch_left, batch_right)
-    loss = compute_loss(pred, batch_y)
-    residual.append(float(loss))
+    # residual = []
+    # for i in range(len(y)):
+    #     batch_y = torch.tensor(y[i], dtype=torch.float32).view(1, 1)
+    #     batch_left = x_left[i]
+    #     batch_left = torch.tensor(batch_left, dtype=torch.float32).view(1, 1, -1)
+    #     batch_right = x_right[i]
+    #     batch_right = torch.tensor(batch_right, dtype=torch.float32).view(1, 1, -1)
+    #     pred = model(batch_left, batch_right)
+    #     loss = compute_loss(pred, batch_y)
+    #     residual.append(float(loss))
 
-residual = np.array(residual)
-peak_index = residual.argmax()
-peak = residual[peak_index]
+    torch.save(model.state_dict(), f'./regression_params/{fname}.pt')
 
-plt.figure(figsize=[20, 5])
-plt.plot(residual)
-plt.scatter([peak_index], [peak], c='red')
-plt.show()
+    # residual = np.array(residual)
+    # peak_index = residual.argmax()
+    # peak = residual[peak_index]
+
+    # plt.figure(figsize=[20, 5])
+    # plt.plot(residual)
+    # plt.scatter([peak_index], [peak], c='red')
+    # plt.show()
