b298156d5147a36d07d99f62c7b2950a1f1f7802
Dennis Lau <dennishylau@gmail.com>
2021-11-30 22:51:32 +0800

    feat: 2nd order diff in abs value

--- src/model/model_setting/second_order_diff.py
+++ src/model/model_setting/second_order_diff.py
@@ -36,7 +36,8 @@ class SecondOrderDiff(BaseModelSetting):
 
     def transform_2nd_order(self, ts: TimeSeries) -> pd.Series:
         '''
-        Returns: a Series with called `2nd_diff` from transformation
+        Returns: a Series with called `2nd_diff` from transformation in absolute
+        value
         '''
         # take differencing
         df = ts.df
@@ -49,27 +50,28 @@ class SecondOrderDiff(BaseModelSetting):
         df_did['series_did1'] = df_did.series_t1 - df_did.series
         series: pd.Series = df_did.series_t2 - 2 * df_did.series_t1 + df_did.series
         series.name = '2nd_diff'
-        return series
+        return series.abs()
 
     def confidence_2nd_diff(
             self,
             ts: TimeSeries,
             diff_series: pd.Series) -> tuple[int, float]:
         '''
-        The weight of the final ensemble, aka the degree of confidence, equals (highest peak of residual) / (2nd higest peak of residual)
+        The weight of the final ensemble, aka the degree of confidence,
+        equals (highest peak of residual) / (2nd higest peak of residual)
         diff_series: the 2nd order diff series
         returns: tuple(index, confidence)
         '''
         anomaly_start = ts.anomaly_start
         # get the 2 largest absolute values
-        abs_series = pd.Series(diff_series[anomaly_start:].abs().unique())
-        values = abs_series.nlargest(2).to_list()
+        unique_series = pd.Series(diff_series[anomaly_start:].unique())
+        values = unique_series.nlargest(2).to_list()
         largest = values[0]
         second = values[1]
         # cal confidence
         conf = largest / second
         # ensure largest is unique
-        largest_series = abs_series[abs_series == largest]
+        largest_series = unique_series[unique_series == largest]
         unique = largest_series.value_counts()[largest] == 1
         if not unique:
             raise ValueError('Largest value is not unique')
