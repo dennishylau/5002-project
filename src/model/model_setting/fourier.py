40189da7e37b5bc5efc9c1e34766e1597182bc67
Hayden2018 <39459225+Hayden2018@users.noreply.github.com>
2021-12-02 23:28:18 +0800

    cherrypick: 88188b51 Update fourier.py

--- src/model/model_setting/fourier.py
+++ src/model/model_setting/fourier.py
@@ -8,28 +8,21 @@ def low_pass(freq, threshold):
     pad = np.array([0 + 0j for _ in range(len(freq)- threshold)])
     return np.concatenate([through, pad])
 
-def high_pass(freq, threshold):
-    through = freq[threshold:]
-    pad = np.array([0 + 0j for _ in range(threshold)])
-    return np.concatenate([pad, through])
-
-def get_fourier_residuals(dataset, threshold):
+def get_fourier_residuals(dataset, period=0, threshold=0):
 
     BASE_PATH = './data-sets/KDD-Cup/data/'
     df = pd.read_csv(BASE_PATH + dataset, names=['values'])
     original = df['values'].values
 
-    plt.plot(original)
-    plt.show()
+    if threshold == 0:
+        threshold = 2 * len(original) / period
 
     frequency = fft(original)
     low_pass_filtered = low_pass(frequency, threshold)
     low_pass_restore = ifft(low_pass_filtered)
-    high_pass_filtered = high_pass(frequency, threshold)
-    high_pass_restore = ifft(high_pass_filtered)
 
-    return np.abs(original - low_pass_restore), np.abs(original - high_pass_restore)
+    return np.abs(original - low_pass_restore)
 
-
-# Example
-# low_pass_residual, high_pass_residual = get_fourier_residuals('007_UCR_Anomaly_4000.txt', 200)
+# low_pass_residual = get_fourier_residuals('012_UCR_Anomaly_15000.txt', threshold=100)
+# plt.plot(low_pass_residual)
+# plt.show()
